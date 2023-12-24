79ee5124e7abbd4de56e256bedb6c5de6490438c
Alireza Andeh <sasan.andeh@gmail.com>
2023-12-24 23:58:31 +0330

    add new view for test

--- EShopANDEH/settings.py
+++ EShopANDEH/settings.py
@@ -130,11 +130,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
-
+"""
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'static'),
     os.path.join(BASE_DIR,'media'),
-]
+]"""
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- core/view/views.py
+++ core/view/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('this test')
+    return render(request , 'base.html',{ })
