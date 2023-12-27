0e1a3793a40c739ba0df9296fe806243de85f3c6
wRITER <sasan.andeh@gmail.com>
2023-12-27 18:19:23 +0330

    change html view

--- core/view/views.py
+++ core/view/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request , 'base.html',{ })
+    return render(request , 'core/index.html',{ })
