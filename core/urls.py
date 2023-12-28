f3d17c7436ad530e9bbd2bac6d412606572f7df3
wRITER <sasan.andeh@gmail.com>
2023-12-28 10:16:43 +0330

    add about url

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from core.view.About import about
 
 
 urlpatterns = [ 
-    path('',index),
-    path('about' , about),
+    path('',index, name='index'),
+    path('about' , about,name='about'),
 ]
