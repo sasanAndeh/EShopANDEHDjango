513993baf94a5aefe890ebab8aedc46ff6889055
wRITER <sasan.andeh@gmail.com>
2023-12-28 12:30:33 +0330

    add models to admin

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.Product import product
+from .models.Category import category
+
 
 # Register your models here.
+
+admin.site.register(product)
+admin.site.register(category)
